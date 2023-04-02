b9992e9a1608f9ca26b59d177fa78f33c2a7b6b5
jparrker <102701917+jparrker@users.noreply.github.com>
2023-04-02 19:49:03 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import List from "./List";
+import data from "./data";
+import { useState } from "react";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
